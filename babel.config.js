52c2747b9f29d39a3981f0f74235dbd00697a4c4
marble <lzh.marble@gmail.com>
2023-02-20 20:21:42 +0800

    feat: realize basic function

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+  presets: [
+    // @see https://babel.dev/docs/en/babel-preset-typescript#options
+    ['@babel/preset-typescript', {
+
+    }]
+  ],
+};
---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import path from "path";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import { babel } from "@rollup/plugin-babel";
+import replace from "@rollup/plugin-replace";
+import pkg from "./package.json";
+
+const production = process.env.NODE_ENV == "prod";
+
+function createConfig() {
+  return {
+    input: path.resolve(__dirname, "src/index.ts"),
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+      },
+      {
+        file: pkg.module,
+        format: "es",
+      },
+    ],
+    plugins: [
+      resolve({
+        browser: false,
+        extensions: [".ts", ".js"],
+      }),
+      commonjs(),
+      replace({
+        "process.env.NODE_ENV": process.env.NODE_ENV,
+        preventAssignment: true,
+      }),
+      babel({
+        include: ["src/**/*"],
+        extensions: [".js", ".ts"],
+        babelHelpers: "bundled",
+      }),
+      production && terser(),
+    ],
+  };
+}
+
+export default createConfig();
